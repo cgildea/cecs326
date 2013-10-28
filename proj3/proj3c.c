efe402ece74841a7ba26a969099fc7903efeb234
Cody Gildea <cbgildea@gmail.com>
2013-10-28 00:19:16 -0700

    Linkws proj3p to proj3c

---
+++ proj3/proj3c.c
@@ -0,0 +1,74 @@
+/**************************************************************************/ 
+/* PROGRAM NAME: proj3c.c                                                 */
+/* CLASS:        CECS-326                                                 */
+/* INSTRUCTOR:   Mr. Haney Williams                                       */
+/* STUDENTS:     Cody Gildea                                              */
+/*               Steven Le                                                */
+/* DESCRIPTION: This program utilizes a parent program and child program  */
+/* to demonstrate using 'locks'  and 'wait' with processes.				  */
+/**************************************************************************/ 
+# include <stdio.h> 
+# include <stdlib.h> 
+# include <sys/types.h> 
+# include <unistd.h>
+# include <sys/wait.h>
+# include<sys/stat.h>
+# include<errno.h>
+# include<fcntl.h>
+main(int argc, char *argv[])
+{
+        pid_t pid;                						// For child process
+        int ret_value, num_tries, sleeptime, i, value;         // Ret_value and sleeptime
+		char *fname;						  			// File name
+		char *lockfname = "lock1";								// Lock file name
+		char sleepChar[3];
+		
+		value = atoi(argv[0]);
+        fname = argv[1];
+        num_tries = atoi(argv[2]);        // Number of tries, argument 2
+        sleeptime = atoi(argv[3]);        // Max sleeptime, argument 3
+
+        pid = getpid();                // Get the process ID
+        srand((unsigned) pid); 
+		
+		for (i = 0; i<num_tries;++i)
+		{
+			if((pid = fork() == 0)
+			{
+				sprintf(sleepChar, "%d", sleeptime);
+				execlp("acquire", "acquire", lockfname, sleepChar, "1", (char*)0);
+			}
+			while ((w=wait(&status)) && w != - 1)
+			{
+                if(w != -1) 
+			}
+			if (status == 0)
+				break;
+			else
+				sleep(rand()%sleeptime);
+		}
+		 if(i == num_tries)
+		 {
+			printf("\nUnable to obtain lockfile\n");
+			exit(value);
+         }
+		 if ((pid = fork() == 0)
+		 {
+			execlp("/bin/cat", "/bin/cat", fname, (char*)0);
+		 }
+		 while ((w=wait(&status)) && w != - 1)
+		{
+            if(w != -1) 
+		}
+		 if ((pid = fork() == 0)
+		 {
+			sprintf(sleepChar, "%d", sleeptime);
+			execlp("release", "release", lockfname, sleepChar, "1", (char*)0);
+		 }
+		 while ((w=wait(&status)) && w != - 1)
+		{
+            if(w != -1) 
+		}
+		 if(status == 0)
+			exit(getpid()&255);
+}
--- proj3/proj3p.c
+++ proj3/proj3p.c
@@ -7,27 +7,31 @@
 /* DESCRIPTION: This program utilizes a parent program and child program  */
 /* to demonstrate using locks  and wait with processes.					  */
 /**************************************************************************/ 
-# include<sys/types.h>
+# include <stdio.h> 
+# include <stdlib.h> 
+# include <sys/types.h> 
+# include <unistd.h>
+# include <sys/wait.h>
 # include<sys/stat.h>
-# include<fcntl.h>
-# include<stdio.h>
-# include<stdlib.h>
 # include<errno.h>
+# include<fcntl.h>
 
 main(int argc, char *argv[])
 {
         pid_t pid, w;                 		  // For child process and wait time
         int k, status, num_tries, sleeptime;  // For child process number, status, number processes, and sleeptime
-        char value[3];                		  // Char string for exec call
+        char *fname;						  // File name
+		char *lockfname = "lock1";			  // Lock file name
+		char value[3];                		  // Char string for exec call
 
         if (argc !=3) // If arguments are not 4 inputs
            { 
                      printf("Invalid inputs, requires 4 arguments.\n", argv[0]); 
                      exit(1); 
            } 
-
-        num_tries = atoi(argv[1]);        // Number of tries, argument 2
-        sleeptime = atoi(argv[2]);        // Max sleeptime, argument 3
+		fname = argv[1];
+        num_tries = atoi(argv[2]);        // Number of tries, argument 2
+        sleeptime = atoi(argv[3]);        // Max sleeptime, argument 3
         
         if (num_tries <= 0)   // If argument n_try is less than or equal to 0, throw exception
         {
@@ -42,15 +46,18 @@ main(int argc, char *argv[])
                 printf("Sleeptime must be > 0\n");
                 exit(1);
         }
+		unlink(lockfname);
         for (k=0;k<3;++k)
         {
                 if((pid = fork()) == 0)        // Create child process
                 {
                         sprintf(value, "%d",k);
-                        execl("proj3c","child", value, argv[2], (char *)0);        // Call child program
+                        execl("proj3c","child", value, argv[1], argv[2], argv[3], (char *)0);        // Call child program
                 }
+				/*
                 else
                         printf("Forked child %d\n",pid);
+				*/
         }
         /* Wait for children */
         while ((w=wait(&status)) && w != - 1)
