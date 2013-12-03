47c8d95c5f9f14e986d8b91a084aa6d01f1c5c39
cgildea <cbgildea@gmail.com>
2013-12-03 13:01:38 -0800

    swapped parent and child

--- lab8/pipe.c
+++ lab8/pipe.c
@@ -1,3 +1,10 @@
+/**************************************************************************/ 
+/* PROGRAM NAME: pipe.c (lab 8)                                           */
+/* CLASS:        CECS-326                                                 */
+/* INSTRUCTOR:   Mr. Haney Williams                                       */
+/* STUDENTS:     Cody Gildea                                              */                                            
+/* DESCRIPTION: This program utilizes creating pipes					  */
+/**************************************************************************/ 
 /* Unnamed pipe pipe.c */ 
 /* Usage: pipe message_to_be_written. Parent write a message to child */ 
 #include <stdio.h>
@@ -7,11 +14,15 @@
 #include <string.h> 
 void main(int argc, char *argv[]) 
 { 
-	int f_des[2]; 
+	int f_des[2]; 					
+	/* Success: 0, failure: -1, sets errno. If successful, pipe returns two 
+	integer file descriptors, filedes[0] and filedes[1], that reference two data streams */
+
 	static char message[BUFSIZ]; 
 	char buffer[MAX_CANON]; 
 	char *c; 
-	int i,k, n; pid_t childpid; 
+	int i,k, n; 
+	pid_t childpid; 
  	if (argc !=2) 
 	{ 
 		printf ("\n Usage: %s message\n", *argv); 
@@ -25,27 +36,34 @@ void main(int argc, char *argv[])
 	} 
  	switch (fork())
 	{ 
- 		case -1: perror ("Fork"); exit(3); 
+ 		case -1: 
+ 			perror ("Fork"); 
+ 			exit(3); 
  		case 0: /* In the child */ 
- 		close(f_des[1]); 
-		if (read(f_des[0], message, BUFSIZ) != -1) 
-		{ 
-			printf ("Message received by child: *%s*\n", message); 
-			fflush(stdout); 
-		} 
- 		else { perror ("Read"); exit(4);} break; 
+ 			close(f_des[0]); 
+		 	if (write(f_des[1], argv[1], strlen(argv[1])) != -1)
+			{ 
+	 			printf ("Message sent by child: [%s]\n", argv[1]);
+				fflush(stdout); 
+			} 
+	 		else 
+			{ 
+				perror ("Write"); 
+				exit(5); 
+			} 
+	 		break; 
  		default: /* In the parent */ 
-	 	close(f_des[0]); 
-	 	if (write(f_des[1], argv[1], strlen(argv[1])) != -1)
-		{ 
- 			printf ("Message sent by parent: [%s]\n", argv[1]);
-			fflush(stdout); 
-		} 
- 		else 
-		{ 
-			perror ("Write"); 
-			exit(5); 
-		} 
+	 		close(f_des[1]); 
+			if (read(f_des[0], message, BUFSIZ) != -1) 
+			{ 
+				printf ("Message received by parent: *%s*\n", message); 
+				fflush(stdout); 
+			} 
+	 		else 
+	 		{ 
+	 			perror ("Read"); 
+	 			exit(4);
+	 		} 	
  	} 
  	exit(0); 
 }
--- lab8/pipe1.c
+++ lab8/pipe1.c
@@ -1,3 +1,10 @@
+/**************************************************************************/ 
+/* PROGRAM NAME: pipe1.c (lab 8)                                          */
+/* CLASS:        CECS-326                                                 */
+/* INSTRUCTOR:   Mr. Haney Williams                                       */
+/* STUDENTS:     Cody Gildea                                              */                                            
+/* DESCRIPTION: This program utilizes creating pipes					  */
+/**************************************************************************/ 
 /* Named pipe pipe1.c */ 
 /* Usage: pipe1 pipename. Child writes a message to parent */ 
 #include <stdio.h>
@@ -15,26 +22,72 @@
 # define BUFSIZE 256 
 void main(int argc, char *argv[]) 
 { 
- mode_t fifo_mode = S_IRUSR | S_IWUSR; int fd, status, child; char buf[BUFSIZE]; unsigned strsize; 
+ 	mode_t fifo_mode = S_IRUSR | S_IWUSR; 
+ 	int fd, status, child; 
+ 	char buf[BUFSIZE]; 
+ 	unsigned strsize; 
  
- if (argc !=2) { printf ("\n Usage: %s pipename\n", argv[0]); exit(1); } 
+ 	if (argc !=2) 
+ 	{ 
+ 		printf ("\n Usage: %s pipename\n", argv[0]); 
+ 		exit(1); 
+ 	} 
  /* generate a named pipe with r/w for user */ 
- if ((mkfifo(argv[1],fifo_mode) == -1) && (errno != EEXIST)) { perror ("Pipe"); exit(1); } 
- if (( child = fork()) == -1) { perror ("Fork"); exit(1); } 
- else if (child == 0){ printf ("\n Child %ld is about to open FIFO %s\n", (long)getpid(), argv[1]); 
- if ((fd = open(argv[1], O_WRONLY)) == -1) {perror("Child cannot open FIFO"); exit(1);} 
- /* In the child */ 
- sprintf (buf, "This was written by child %ld\n", (long)getpid()); strsize = strlen(buf) + 1; 
- if (write(fd, buf, strsize) != strsize) { printf("Child write to FIFO failed\n"); exit(1); } 
- printf ("Child %ld is done\n", (long)getpid()); 
- } else { /* parent does a read */ 
- printf ("Parent %ld is about to open FIFO %s\n", (long) getpid(), argv[1]); 
- if ((fd = open(argv[1], O_RDONLY | O_NONBLOCK)) == -1) { perror("Parent cannot open FIFO"); 
-exit(1); } 
- printf ("Parent is about to read\n", (long)getpid()); 
- while ((wait(&status) == -1) && (errno == EINTR)); 
- if (read(fd, buf, BUFSIZE) <=0) { perror("Parent read from FIFO failed\n");exit(1); } 
- printf ("Parent %ld received: %s\n", (long)getpid(), buf); 
- } 
- exit(0); 
+ 	if ((mkfifo(argv[1],fifo_mode) == -1) && (errno != EEXIST)) /* EEXIST -> file already exists*/
+ 		/* success: 0, failure: -1, sets errno. mkfifo 
+		creates the FIFO file referenced by path; mode: File mode bits: 
+		S_IRUSR read permission, owner. S_IWUSR write permission, owner */
+ 	{ 
+ 		perror ("Pipe"); 
+ 		exit(1); 
+ 	} 
+ 	if (( child = fork()) == -1) 
+ 	{ 
+ 		perror ("Fork"); 
+ 		exit(1); 
+ 	} 
+ 	else if (child == 0)
+ 	{ 
+ 		printf ("\n Child %ld is about to open FIFO %s\n", (long)getpid(), argv[1]); 
+ 		if ((fd = open(argv[1], O_WRONLY)) == -1) /* Open for writing only*/
+ 		{
+ 			perror("Child cannot open FIFO"); 
+ 			exit(1);
+ 		} 
+		/* In the child */ 
+	 	sprintf (buf, "This was written by child %ld\n", (long)getpid()); 
+	 	strsize = strlen(buf) + 1; 
+	 	if (write(fd, buf, strsize) != strsize) 
+	 		/* success: number of bytes written, 
+			failure: -1, sets errno. Write nbyte bytes from the buffer 
+			referenced by buf using the file descriptor pecified by filedes. */
+	 	{ 
+	 		printf("Child write to FIFO failed\n"); 
+	 		exit(1); 
+	 	} 
+	 	printf ("Child %ld is done\n", (long)getpid()); 
+ 	} 
+ 	else 
+ 	{ 
+ 	/* parent does a read */ 
+	 	printf ("Parent %ld is about to open FIFO %s\n", (long) getpid(), argv[1]); 
+	 	if ((fd = open(argv[1], O_RDONLY | O_NONBLOCK)) == -1) /* Open for reading only*/
+		{ 
+	 		perror("Parent cannot open FIFO"); 
+			exit(1); 
+		} 
+	 	printf ("Parent is about to read\n", (long)getpid()); 
+	 	while ((wait(&status) == -1) && (errno == EINTR)); /* EINTR -> interrupted system call*/
+		 	if (read(fd, buf, BUFSIZE) <=0) 
+		 	/* success: number of bytes read, failure: -1, sets errno. 
+		 	All reads are initiated from current position. Read nbyte bytes 
+		 	from the open file associated with the file descriptor filedes 
+		 	into the buffer referenced by buf. */
+		 	{ 
+		 		perror("Parent read from FIFO failed\n");
+		 		exit(1); 
+		 	} 
+	 	printf ("Parent %ld received: %s\n", (long)getpid(), buf); 
+ 	} 
+ 	exit(0); 
 }
