84fb6f523f0594554d041e90babd89c2e2e630eb
cgildea <cbgildea@gmail.com>
2013-12-10 01:08:27 -0800

    added comments

--- proj6/proj6.c
+++ proj6/proj6.c
@@ -23,19 +23,21 @@
 # define BUFSIZE 256 
 void main(int argc, char *argv[]) 
 { 
-    mode_t fifo_mode = S_IRUSR | S_IWUSR; 
-    int fd, status, child, order, i, j; 
-    char buf[BUFSIZE]; 
-    unsigned strsize; 
-    char *inputMessage;
-    char *pipeName;
+    mode_t fifo_mode = S_IRUSR | S_IWUSR;       // Declare the mode for the pipe (FIFO)
+    int fd, status, child, order, i, j;         
+    char buf[BUFSIZE];                          // For the buffer to send through the pipe
+    unsigned strsize;                           // For a string size
+    char *inputMessage;                         // For the input message
+    char *pipeName;                             // For pipe name
     
     if (argc < 4 || argc > 5) 
+    /* Check that there is 4 or 5 arguments */
     { 
         printf ("\n Invalid input. Arguments must be 4 or 5\n"); 
         exit(1);
     }
     if(strcmp(argv[1], "n") == 0)
+    /* If argument 2 is 'n' for named pipe */
     {
         if (argc != 5) 
         /* Check input arguments are exactly 5 */
@@ -44,6 +46,7 @@ void main(int argc, char *argv[])
             exit(1);
         }
         if (strlen(argv[4]) > 32)
+        /* If the message is greater than 32 */
         {
             printf ("\nInvalid input. The message cannot have a length greater than 32.\n"); 
             exit(1);
@@ -57,13 +60,14 @@ void main(int argc, char *argv[])
             order = 1;
         }
         else
+        /* Throw error if the 4th argument is not 'c' or 'p' */
         {
             printf("\nInvalid input for the order of the pipe.\n");
             exit(1);
         }
         
-        pipeName = argv[2];
-        inputMessage = argv[4];
+        pipeName = argv[2];             // Set the pipeName to the 3rd argument if 'n' is an option
+        inputMessage = argv[4];         // Set the inputMessage to the 5th argument
     }
     else if(strcmp(argv[1], "u") == 0)
     {
@@ -74,6 +78,7 @@ void main(int argc, char *argv[])
             exit(1);
         } 
         if (strlen(argv[3]) > 32)
+        /* If the message is greater than 32 */
         {
             printf ("\nInvalid input. The message cannot have a length greater than 32.\n"); 
             exit(1);
@@ -88,19 +93,22 @@ void main(int argc, char *argv[])
         }
         else
         {
+        /* Throw error if the 4th argument is not 'c' or 'p' */
             printf("\nInvalid input for the order of the pipe.\n");
             exit(1);
         }
-        inputMessage = argv[3];
-        pipeName = "FIFO";
+
+        inputMessage = argv[3];         // Set the inputMessage to the 4th argument      
+        pipeName = "FIFO";              // Set the pipeName to a defaul anme 'FIFO' if 'u' is an option
     }
     else
+    /* Throw an error if the second argument is not 'n' or 'u' */
     {
         printf("\nInvalid input for the second argument. %s should be 'n' or 'u'.\n", argv[1]);
         exit(1);
     }
 
-    /* generate a named pipe with r/w for user */ 
+    /* generate a named pipe with r/w for user */  /* Make a FIFO special file */ 
     if ((mkfifo(pipeName,fifo_mode) == -1) && (errno != EEXIST)) /* EEXIST -> file already exists*/
         /* success: 0, failure: -1, sets errno. mkfifo 
         creates the FIFO file referenced by path; mode: File mode bits: 
@@ -126,6 +134,7 @@ void main(int argc, char *argv[])
                 perror("\nChild cannot open FIFO"); 
                 exit(1);
             } 
+            /* Run through the inputMessage and change its case to the opposite of what was inputed */
             for(i=0;i<=strlen(inputMessage);i++)
             {
                 if(inputMessage[i]>=65&&inputMessage[i]<=90)
@@ -150,7 +159,6 @@ void main(int argc, char *argv[])
         { 
 
         /* parent does a read */ 
-            //printf ("Parent %ld is about to open FIFO %s\n", (long) getpid(), argv[1]); 
             if ((fd = open(pipeName, O_RDONLY | O_NONBLOCK)) == -1) /* Open for reading only*/
             { /* Open error*/
                 perror("\nParent cannot open FIFO"); 
@@ -208,6 +216,7 @@ void main(int argc, char *argv[])
                 perror("\nParent cannot open FIFO"); 
                 exit(1);
             } 
+            /* Run through the inputMessage and change its case to the opposite of what was inputed */
             for(i=0;i<=strlen(inputMessage);i++)
             {
                 if(inputMessage[i]>=65&&inputMessage[i]<=90)
@@ -215,7 +224,7 @@ void main(int argc, char *argv[])
                 else if (inputMessage[i]>=97&&inputMessage[i]<=122)
                     inputMessage[i]=inputMessage[i]-32;
             }
-            /* In the child */ 
+            /* In the parent */ 
             sprintf (buf, "*%s* from parent", inputMessage); 
             strsize = strlen(buf) + 1; 
             if (write(fd, buf, strsize) != strsize) 
@@ -228,7 +237,6 @@ void main(int argc, char *argv[])
             } 
             printf ("\nMessage sent.\n"); 
         }
-
     }
     exit(0); 
 }
